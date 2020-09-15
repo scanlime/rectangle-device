9750c74d0d949bb851d3a9bb9db51d679991c1a0
Micah Elizabeth Scott <micah@scanlime.org>
2020-09-15 00:53:03 -0700

    big dht limits

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use libp2p::gossipsub::{self, Gossipsub, GossipsubConfigBuilder, MessageAuthenti
 use libp2p::gossipsub::error::PublishError;
 use libp2p::identify::{Identify, IdentifyEvent};
 use libp2p::kad::{self, Kademlia, KademliaEvent, KademliaConfig};
-use libp2p::kad::record::store::MemoryStore;
+use libp2p::kad::record::store::{MemoryStore, MemoryStoreConfig};
 use libp2p::mdns::{Mdns, MdnsEvent};
 use libp2p::ping::{Ping, PingConfig, PingEvent};
 use libp2p::swarm::{SwarmEvent, NetworkBehaviourEventProcess, NetworkBehaviour};
@@ -231,6 +231,13 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for P2PVideoBehaviour {
     }
 }
 
+fn kad_store_config() -> MemoryStoreConfig {
+    let mut config: MemoryStoreConfig = Default::default();
+    config.max_records = 128*1024;
+    config.max_provided_keys = 128*1024;
+    config
+}
+
 impl P2PVideoNode {
     fn new(block_receiver : Receiver<BlockType>) -> Result<P2PVideoNode, Box<dyn Error>> {
         let local_key = identity::Keypair::generate_ed25519();
@@ -238,12 +245,8 @@ impl P2PVideoNode {
         let gossipsub_topic = gossipsub::Topic::new("rectangle-net".into());
         let transport = libp2p::build_development_transport(local_key.clone())?;
         let mut kad_config : KademliaConfig = Default::default();
-        let mut kad_store_config : MemoryStoreConfig = Default::default();
         const KAD_LAN : &[u8] = b"/ipfs/lan/kad/1.0.0";
         const KAD_WAN : &[u8] = b"/ipfs/wan/kad/1.0.0";
-
-        kad_store_config.max_records = 128*1024;
-        kad_store_config.max_provided_keys = 128*1024;
 
         let mut behaviour = P2PVideoBehaviour {
             gossipsub: Gossipsub::new(
@@ -259,11 +262,11 @@ impl P2PVideoNode {
             bitswap: Bitswap::new(),
             kad_lan: Kademlia::with_config(
                 local_peer_id.clone(),
-                MemoryStore::with_config(local_peer_id.clone(), kad_store_config.clone()),
+                MemoryStore::with_config(local_peer_id.clone(), kad_store_config()),
                 kad_config.set_protocol_name(Cow::Borrowed(KAD_LAN)).clone()),
             kad_wan: Kademlia::with_config(
                 local_peer_id.clone(),
-                MemoryStore::with_config(local_peer_id.clone(), kad_store_config.clone()),
+                MemoryStore::with_config(local_peer_id.clone(), kad_store_config()),
                 kad_config.set_protocol_name(Cow::Borrowed(KAD_WAN)).clone()),
             mdns: Mdns::new()?,
             peer_id: local_peer_id.clone(),
