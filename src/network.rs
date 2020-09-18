d39a13bec281683c898c6a719cd91db4c6d474f3
Micah Elizabeth Scott <micah@scanlime.org>
2020-09-18 16:37:22 -0700

    tidy keypair generation a little

--- src/network.rs
+++ src/network.rs
@@ -10,10 +10,11 @@ use libipld::cid::Cid;
 use async_std::task::{Poll, Context};
 use libipld::multihash::Multihash;
 use libp2p_bitswap::{Bitswap, BitswapEvent};
-use libp2p::{identity, PeerId, Swarm, NetworkBehaviour};
+use libp2p::{PeerId, Swarm, NetworkBehaviour};
 use libp2p::core::multiaddr::{Multiaddr, Protocol};
 use libp2p::gossipsub::{self, Gossipsub, GossipsubConfigBuilder, MessageAuthenticity, GossipsubEvent};
 use libp2p::gossipsub::error::PublishError;
+use libp2p::identity::Keypair;
 use libp2p::identify::{Identify, IdentifyEvent};
 use libp2p::kad::{self, Kademlia, KademliaEvent, KademliaConfig};
 use libp2p::kad::record::store::{MemoryStore, MemoryStoreConfig};
@@ -169,26 +170,34 @@ fn kad_store_config() -> MemoryStoreConfig {
     config
 }
 
+fn keypair_from_openssl_rsa() -> Result<Keypair, Box<dyn Error>> {
+    // This is a temporary hack.
+    // I'm using an RSA keypair because the new elliptic curve
+    // based identity seems to break p2p-circuit routing in go-ipfs?
+
+    log::info!("generating key pair");
+
+    let mut genpkey = Command::new("openssl")
+        .arg("genpkey").arg("-algorithm").arg("RSA")
+        .arg("-pkeyopt").arg("rsa_keygen_bits:2048")
+        .arg("-pkeyopt").arg("rsa_keygen_pubexp:65537")
+        .stdout(Stdio::piped())
+        .spawn().unwrap();
+
+    let pkcs8 = Command::new("openssl")
+        .arg("pkcs8").arg("-topk8").arg("-nocrypt")
+        .arg("-outform").arg("der")
+        .stdin(Stdio::from(genpkey.stdout.take().unwrap()))
+        .output().unwrap();
+
+    let mut data = pkcs8.stdout.as_slice().to_vec();
+    Ok(Keypair::rsa_from_pkcs8(&mut data)?)
+}
+
 impl P2PVideoNode {
     pub fn new(block_receiver : Receiver<BlockInfo>) -> Result<P2PVideoNode, Box<dyn Error>> {
 
-        let local_key = {
-            // Using RSA keypair because the new elliptic curve based identity seems to break p2p-circuit routing
-            log::info!("generating key pair");
-            let mut genpkey = Command::new("openssl")
-                .arg("genpkey").arg("-algorithm").arg("RSA")
-                .arg("-pkeyopt").arg("rsa_keygen_bits:2048")
-                .arg("-pkeyopt").arg("rsa_keygen_pubexp:65537")
-                .stdout(Stdio::piped())
-                .spawn().unwrap();
-            let pkcs8 = Command::new("openssl")
-                .arg("pkcs8").arg("-topk8").arg("-nocrypt")
-                .arg("-outform").arg("der")
-                .stdin(Stdio::from(genpkey.stdout.take().unwrap()))
-                .output().unwrap();
-            let mut data = pkcs8.stdout.as_slice().to_vec();
-            identity::Keypair::rsa_from_pkcs8(&mut data)?
-        };
+        let local_key = keypair_from_openssl_rsa()?;
         let local_peer_id = PeerId::from(local_key.public());
         log::info!("local identity is {}", local_peer_id.to_string());
 
