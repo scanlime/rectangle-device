e025edae47dd4d99f5975140f6de4c74798dfd47
Micah Elizabeth Scott <micah@scanlime.org>
2020-09-19 01:36:50 -0700

    fix for accidentally disposable warmers

--- src/warmer.rs
+++ src/warmer.rs
@@ -4,6 +4,7 @@ use async_std::sync::Receiver;
 use std::time::Duration;
 use reqwest::{Client, StatusCode};
 
+#[derive(Clone)]
 pub struct Warmer {
     pub url_receiver: Receiver<String>,
 }
@@ -15,33 +16,42 @@ const TIMEOUT_MSEC: u64 = 2500;
 impl Warmer {
     pub async fn task(self) {
         let mut tasks = vec![];
-        for warmer_id in 0..POOL_SIZE {
-            let url_receiver = self.url_receiver.clone();
-            let client = Client::builder()
-                .timeout(Duration::from_millis(TIMEOUT_MSEC))
-                .build().unwrap();
+        for pool_id in 0..POOL_SIZE {
+            let pool_clone = self.clone();
             tasks.push(tokio::spawn(async move {
-                loop {
-                    let url = url_receiver.recv().await.unwrap();
-                    for try_num in 0..NUM_RETRIES {
-                        log::trace!("[{}] head {} try {}", warmer_id, url, try_num);
-                        let result = client.head(&url).send().await;
-                        match result.map(|r| r.status()) {
-                            Ok(StatusCode::OK) => {
-                                log::info!("[{}] try# {}, {}", warmer_id, try_num, url);
-                                return;
-                            },
-                            err => {
-                                log::trace!("[{}] {:?}", warmer_id, err);
-                            }
-                        };
-                    }
-                    log::warn!("[{}] failed {} after {} tries", warmer_id, url, NUM_RETRIES);
-                }
-            }));
+                pool_clone.pool_task(pool_id).await;
+            }))
         }
         for task in tasks {
             task.await.unwrap();
         }
+    }
+
+    async fn pool_task(&self, pool_id: usize) {
+        let client = Client::builder()
+            .timeout(Duration::from_millis(TIMEOUT_MSEC))
+            .build().unwrap();
+
+        loop {
+            self.pool_task_poll(pool_id, &client).await;
+        }
+    }
+
+    async fn pool_task_poll(&self, pool_id: usize, client: &Client) {
+        let url = self.url_receiver.recv().await.unwrap();
+        for try_num in 0..NUM_RETRIES {
+            log::trace!("[{}] head {} try {}", pool_id, url, try_num);
+            let result = client.head(&url).send().await;
+            match result.map(|r| r.status()) {
+                Ok(StatusCode::OK) => {
+                    log::info!("[{}] try# {}, {}", pool_id, try_num, url);
+                    return;
+                },
+                err => {
+                    log::trace!("[{}] {:?}", pool_id, err);
+                }
+            };
+        }
+        log::warn!("[{}] failed {} after {} tries", pool_id, url, NUM_RETRIES);
     }
 }
