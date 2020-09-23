7775b0c5aa718141040f66daaeca45451aebce57
Micah Elizabeth Scott <micah@scanlime.org>
2020-09-23 12:23:46 -0700

    build node modules into cargo out_dir too

--- player/build.rs
+++ player/build.rs
@@ -1,9 +1,16 @@
 use std::process::Command;
 use build_deps::rerun_if_changed_paths;
+use std::path::Path;
+use std::env;
 
 fn main() {
-    Command::new("yarn").arg("install").status().unwrap();
-    Command::new("yarn").arg("build").status().unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let node_modules = Path::new(&out_dir).join("node_modules");
+    let yarn_args = vec![ "--modules-folder", node_modules.to_str().unwrap() ];
+
+    Command::new("yarn").args(&yarn_args).arg("install").status().unwrap();
+    Command::new("yarn").args(&yarn_args).arg("build").status().unwrap();
+
     rerun_if_changed_paths( "package.json" ).unwrap();
     rerun_if_changed_paths( "webpack.config.js" ).unwrap();
     rerun_if_changed_paths( "src/**" ).unwrap();
