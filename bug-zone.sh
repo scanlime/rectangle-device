9a4fa411ed7c6260869cdbbfb28e02a00b74024c
Micah Elizabeth Scott <micah@scanlime.org>
2020-09-26 19:04:41 -0700

    --long --options

--- bug-zone.sh
+++ bug-zone.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cargo run \
 	-- \
-	-i 5s \
-	-G http://99.149.215.66:8080 \
-	-g https://dweb.link \
-	-r /ip4/99.149.215.66/tcp/4001/p2p/QmPjtoXdQobBpWa2yS4rfmHVDoCbom2r2SMDTUa1Nk7kJ5 \
+	--interval 5s \
+	--pin-gw http://99.149.215.66:8080 \
+	--pub-gw https://dweb.link \
+	--router /ip4/99.149.215.66/tcp/4001/p2p/QmPjtoXdQobBpWa2yS4rfmHVDoCbom2r2SMDTUa1Nk7kJ5 \
 	-- \
 	-i https://live.diode.zone/hls/eyesopod/index.m3u8 \
 	-c copy
